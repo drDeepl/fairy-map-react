fece4b983b5b7f1bb156a83397f9500b5c685172
drDeepl <vlimaster@yandex.ru>
2024-10-04 12:04:08 +0700

    feat:(store): init app store

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import AppSidebar from "../components/Sidebar/Sidebar";
 import Topbar from "../components/Topbar/Topbar";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store";
+import { ProgressSpinner } from "primereact/progressspinner";
+import { Button } from "primereact/button";
 
 interface MainLayoutProps {
   children?: React.ReactNode;
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const isLoad: boolean = useSelector((state: RootState) => state.app.isLoad);
   const [sidebarVisible, setVisible] = useState(false);
+
   return (
     <div>
       <Topbar onClickAvatar={() => setVisible(!sidebarVisible)} />
@@ -16,6 +22,8 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         setVisible={setVisible}
         appendTo={document.getElementById("topbar-mainlayout") as HTMLElement}
       />
+      {isLoad ? <ProgressSpinner /> : null}
+      {/* TODO: Change state after click btn */}
       {children}
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
-import { store } from "./store/store";
+import store from "./store/index";
 import { Provider } from "react-redux";
 import "./assets/index.scss";
 import MainLayout from "./layout/MainLayout";
